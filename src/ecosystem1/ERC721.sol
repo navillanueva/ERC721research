60b9c6cb11d091475c3f6ac7983631c22afac19c
Nicolas <nicolasarnedo@sdx.com>
2024-04-15 20:58:08 +0200

    feat: added erc20

---
+++ src/ecosystem1/ERC721.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyNFTContract is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("MyNFTContract", "NFT") {}
+
+    function mintNFT(address recipient, string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+}
