831e121d7b00bd9b3482ebe7f34e5b43ddc59c79
Nicolas <nicolasarnedo@sdx.com>
2024-04-16 20:32:49 +0200

    feat: added correct contracts

--- src/ecosystem1/ERC721.sol
+++ src/ecosystem1/ERC721.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.0;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC2981} from "@openzeppelin/contracts/token/common/ERC2981.sol";
+import {BitMaps} from "@openzeppelin/contracts/utils/structs/BitMaps.sol";
 
-contract MyNFTContract is ERC721 {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+contract NavillaNFT is ERC721, ERC2981, BitMap{
 
+    uint16 public constant SUPPLY = 1000;
+    
     constructor() ERC721("MyNFTContract", "NFT") {}
 
     function mintNFT(address recipient, string memory tokenURI) public returns (uint256) {
